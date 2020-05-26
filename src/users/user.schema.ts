565c449a968b627dbe232f8526cb6ab6514e42b1
Antonio Spinelli <antonio.86.spinelli@gmail.com>
2020-05-26 21:41:27 +0200

    Add create user and user schema

---
+++ src/users/user.schema.ts
@@ -0,0 +1,13 @@
+import { Schema } from 'mongoose'
+
+export const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+})
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,4 +1,7 @@
+import { MongooseModule } from '@nestjs/mongoose'
 import { Test, TestingModule } from '@nestjs/testing'
+
+import { UserSchema } from './user.schema'
 import { UsersService } from './users.service'
 
 describe('UsersService', () => {
@@ -6,6 +9,10 @@ describe('UsersService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forRoot(process.env.MONGO_URL),
+        MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+      ],
       providers: [UsersService],
     }).compile()
 
@@ -14,5 +21,15 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  it('should create User', async () => {
+    const user = await service.create({
+      email: 'info@test.it',
+      password: '123',
+    })
+    expect(user).not.toBeNull()
+    expect(user.email).toBeTruthy()
+    expect(user.password).toBeTruthy()
   })
 })
